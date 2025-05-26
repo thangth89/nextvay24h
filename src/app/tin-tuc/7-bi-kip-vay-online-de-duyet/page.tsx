a45321059dad803542163ce16e3a6bdcc8064a54
thangth89 <thangth.vinfast@gmail.com>
2025-05-26 23:47:46 +0700

    Update page.tsx

--- src/app/tin-tuc/7-bi-kip-vay-online-de-duyet/page.tsx
+++ src/app/tin-tuc/7-bi-kip-vay-online-de-duyet/page.tsx
@@ -19,7 +19,7 @@ export default function BiKipVayOnlinePage() {
       <div style={{ display: "flex", gap: "20px", maxWidth: "1200px", margin: "0 auto", padding: "20px" }}>
         {/* Nội dung bài viết */}
         <main style={{ flex: 3 }}>
-          <div className="news-content"  style={{backgroundColor: "#fff", borderRadius: "8px" }} > 
+          <div className="news-content"  style={{backgroundColor: "#fff", borderRadius: "8px", padding: "16px" }} > 
             <div
           style={{ color: "#4a4a4a", lineHeight: 1.6, marginTop: 8 }}
            >
