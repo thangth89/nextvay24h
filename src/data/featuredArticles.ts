6a6e61f90e419a58c68026ba205117ddf8ca810c
thangth89 <thangth.vinfast@gmail.com>
2025-06-03 00:16:58 +0700

    Update featuredArticles.ts

--- src/data/featuredArticles.ts
+++ src/data/featuredArticles.ts
@@ -4,13 +4,20 @@ import { FeaturedArticle } from '../types/article';
 export const featuredArticles: FeaturedArticle[] = [
   {
     id: '1',
+    title: 'CIC là gì? Hướng dẫn chi tiết kiểm tra CIC miễn phí và cải thiện điểm tín dụng 2025',
+    slug: '/tin-tuc/CIC-la-gi',
+    thumbnail: '/news/CIC-la-gi.webp',
+    alt: 'CIC là gì? Hướng dẫn chi tiết kiểm tra CIC miễn phí và cải thiện điểm tín dụng 2025'
+  },
+  {
+    id: '2',
     title: 'Top 5 App Vay Uy Tín Không App Đen',
     slug: '/tin-tuc/top-5-app-vay-uy-tin',
     thumbnail: '/news/top-5-vay-uy-tin.webp',
     alt: 'Top 5 App Vay Uy Tín'
   },
   {
-    id: '2',
+    id: '3',
     title: '7 Bí Kíp Vay Online Dễ Duyệt 2025',
     slug: '/tin-tuc/7-bi-kip-vay-online-de-duyet',
     thumbnail: '/news/7-bi-kip-vay-online-de-duyet.webp',
@@ -18,17 +25,10 @@ export const featuredArticles: FeaturedArticle[] = [
   },
   // Thêm bài viết mới ở đây
   {
-    id: '3',
+    id: '4',
     title: 'VayVND là gì? Đánh giá chi tiết ứng dụng vay tiền online VayVND 2025',
     slug: '/tin-tuc/vayvnd-danh-gia-2025',
     thumbnail: '/news/vayvnd-danh-gia-2025.webp',
-    alt: 'Vay Tiền Không Thế Chấp'
-  },
-  {
-    id: '4',
-    title: 'App Đen Là Gì? Vì Sao Nên Tránh',
-    slug: '/tin-tuc/vay-app-den-la-gi',
-    thumbnail: '/news/vay-app-den-la-gi.webp',
     alt: 'Vay Tiền Không Thế Chấp'
   },
   {
