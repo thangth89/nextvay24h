d10a30dc630a446a9cd67bcd30ea419535a9414b
thangth89 <thangth.vinfast@gmail.com>
2025-06-15 12:37:44 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,21 @@ const nextConfig = {
       },
     ];
   },
-
+   {
+        source: '/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+        ],
+        destination: 'https://vay24h.pro.vn/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
   // Cấu hình bảo mật headers
   async headers() {
     return [
