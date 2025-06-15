04842fb99b6c7512f4caf58376d7f6965d3fdb46
thangth89 <thangth.vinfast@gmail.com>
2025-06-15 11:38:16 +0700

    Add files via upload

--- src/app/tin-tuc/page.tsx
+++ src/app/tin-tuc/page.tsx
@@ -1,5 +1,23 @@
-import { redirect } from 'next/navigation';
+import { Article } from "@/components/DanhSachTin";
+import fs from "fs";
+import path from "path";
+import DanhSachTin from "@/components/DanhSachTin";
 
-export default function TinTucRedirect() {
-  redirect('/tin-tuc/trang/1');
+export const metadata = {
+  title: "Tin tức vay tiền online",
+  description: "Danh sách bài viết tài chính, vay online, thẻ tín dụng",
+  alternates: {
+    canonical: "https://vay24h.pro.vn/tin-tuc",
+  },
+};
+
+const PER_PAGE = 6;
+
+export default async function Page() {
+  const filePath = path.join(process.cwd(), "src/data/news.json");
+   const raw = await fs.promises.readFile(filePath, "utf8");
+  const newsList: Article[] = JSON.parse(raw);
+  const totalPages = Math.ceil(newsList.length / PER_PAGE);
+  const articles = newsList.slice(0, PER_PAGE);
+  return <DanhSachTin articles={articles} currentPage={1} totalPages={totalPages} />;
 }
