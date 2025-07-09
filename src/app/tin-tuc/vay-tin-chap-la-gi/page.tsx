4cdbb231fdcf525ffada6602f806c81ee0502eed
thangth89 <thangth.vinfast@gmail.com>
2025-07-10 00:02:41 +0700

    Update page.tsx

--- src/app/tin-tuc/vay-tin-chap-la-gi/page.tsx
+++ src/app/tin-tuc/vay-tin-chap-la-gi/page.tsx
@@ -338,6 +338,7 @@ export default function UnsecuredLoanPage() {
               	position={17}
                 category="loan_apps"
               >
+              <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7l5 5m0 0l-5 5m5-5H6"></path></svg>
                 VAY ONLINE VỚI JEFF
             </AffiliateButton>
           </div>
