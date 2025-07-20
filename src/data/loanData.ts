383bef86d511800768433efca6202b861d924b9c
thangth89 <thangth.vinfast@gmail.com>
2025-07-21 00:57:33 +0700

    Add files via upload

---
+++ src/data/loanData.ts
@@ -0,0 +1,208 @@
+// app/data/loanData.ts
+
+export const loanApps = [
+  {
+    topLabel: "TOP #1",
+    imageSrc: "/vay/jeff.webp",
+    altText: "Jeff Web app vay tiền",
+    title: "Jeff Web",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "1.000.000 – 30.000.000",
+    features: [
+      "<strong>Kết nối hơn 25 đối tác</strong> cho vay",
+      "Thời hạn vay: 3 – 12 tháng",
+      "<strong>Lãi suất: 0% khoản vay đầu</strong>",
+      "Độ tuổi: 22 tuổi trở lên",
+      "Hồ sơ chỉ cần CMND"
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=161",
+    label: "Jeff"
+  },
+  {
+    topLabel: "TOP #2",
+    imageSrc: "/vay/vayvnd.webp",
+    altText: "VayVND ứng dụng vay tiền",
+    title: "VAYVND",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "500.000 – 15.000.000",
+    features: [
+      "Thời hạn vay: 10 – 182 ngày",
+      "<strong>Lãi suất từ 0% – 1,5%/tháng</strong>",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ chỉ cần CMND",
+      "<strong>Xét duyệt: 15 phút</strong>"
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=277",
+    label: "VayVND"
+  },  
+  {
+    topLabel: "TOP #3",
+    imageSrc: "/vay/meocat.webp",
+    altText: "MoneyCat vay tiền online",
+    title: "MONEYCAT",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "500.000 – 10.000.000",
+    features: [
+      "Thời hạn vay: 91 – 182 ngày",
+      "<strong>Lãi suất từ 1,0% - 1,52%/tháng</strong>",
+      "<strong>0% lãi suất cho khách mới</strong>",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ chỉ cần CMND",
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=762",
+    label: "MoneyCat"
+  },
+  {
+    topLabel: "TOP #4",
+    imageSrc: "/vay/moneyveo.webp",
+    altText: "MoneyVeo app vay",
+    title: "MONEYVEO",
+    duyetText: "📌 TỰ ĐỘNG XÉT DUYỆT",
+    amount: "500.000 – 25.000.000",
+    features: [
+      "Thời hạn vay: linh động gia hạn",
+      "<strong>Lãi suất chỉ từ 1,52%/tháng</strong>",
+      "Hệ thống làm việc 24/7",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ chỉ cần CMND",
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=663",
+    label: "MoneyVeo"
+  },
+  {
+    topLabel: "TOP #5",
+    imageSrc: "/vay/cayvang.webp",
+    altText: "Cây Vàng vay tiền",
+    title: "CAYVANG",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "500.000 – 10.000.000",
+    features: [
+      "Thời hạn vay: theo từng gói vay",
+      "<strong>Lãi suất: 0% khoản vay đầu</strong>",
+      "Phí xử lý hồ sơ: 50.000-1.000.000",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ chỉ cần CMND",
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=935",
+    label: "CayVang"
+  },
+  {
+    topLabel: "TOP #6",
+    imageSrc: "/vay/tinvay.webp",
+    altText: "TinVay ứng dụng vay tiền",
+    title: "TINVAY",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "5.000.000– 90.000.000",
+    features: [
+      "Thời hạn vay: 3 – 36 tháng",
+      "<strong>Lãi suất: chỉ từ 1,67%/tháng</strong>",
+      "Xét duyệt nhanh chóng",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ: dùng Sim Viettel và CMND",
+    ],
+    affiliateLink: "https://dinos.scaletrk.com/click?a=11826&o=769",
+    label: "TinVay"
+  },
+  {
+    topLabel: "TOP #7",
+    imageSrc: "/vay/vimayman.webp",
+    altText: "Ví May Mắn app vay tiền",
+    title: "Ví May Mắn",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "1.000.000– 30.000.000",
+    features: [
+      "Thời hạn vay: 91 - 180 ngày",
+      "<strong>Lãi suất: chỉ từ 1,25%/tháng</strong>",
+      "Xét duyệt nhanh",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ: chỉ cần CMND",
+    ],
+    affiliateLink: "https://adpvn.top/7Xwi",
+    label: "ViMayMan"
+  },
+  {
+    topLabel: "TOP #8",
+    imageSrc: "/vay/dong247.webp",
+    altText: "Dong247 vay tiền nhanh",
+    title: "dong247",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "500.000 – 30.000.000",
+    features: [
+      "Thời hạn vay: 91 – 120 ngày",
+      "<strong>Lãi suất: 0% - 1,25%/tháng</strong>",
+      "Kết nối hơn 25 đối tác cho vay",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ: chỉ cần CMND",
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=302",
+    label: "Dong247"
+  },
+  {
+    topLabel: "TOP #9",
+    imageSrc: "/vay/finapps.webp",
+    altText: "FinApps ứng dụng vay tiền",
+    title: "FinApps",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "300.000 – 20.000.000",
+    features: [
+      "Thời hạn vay: 3 – 12 tháng",
+      "<strong>Lãi suất: 0% khoản vay đầu</strong>",
+      "Xử lý hồ sơ 24/7",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ: chỉ cần CMND",
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=821",
+    label: "FinApps"
+  },
+  {
+    topLabel: "TOP #10",
+    imageSrc: "/vay/visame.webp",
+    altText: "ViSame app vay tiền online",
+    title: "VISAME",
+    duyetText: "📌 DUYỆT VAY NHANH",
+    amount: "1.000.000 – 15.000.000",
+    features: [
+      "Thời hạn vay: 3 – 24 tháng",
+      "<strong>Lãi suất: 0% - 1,67%/tháng</strong>",
+      "Hoạt động 24/7",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ: chỉ cần CMND",
+    ],
+    affiliateLink: "https://adpvn.top/pDUW",
+    label: "ViSame"
+  },
+  {
+    topLabel: "NEW",
+    imageSrc: "/vay/finami.webp",
+    altText: "Finami vay tiền bao nợ xấu",
+    title: "Finami",
+    duyetText: "📌 BAO NỢ XẤU",
+    amount: "500.000 – 10.000.000",
+    features: [
+      "Thời hạn vay: 1 – 12 tháng",
+      "<strong>Lãi suất: 0% nếu trả ngày đầu</strong>",
+      "Hoạt động 24/7",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ: chỉ cần CMND",
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=683",
+    label: "Finami"
+  },
+  {
+    topLabel: "NEW",
+    imageSrc: "/vay/credityes.webp",
+    altText: "CreditYes vay tiền nhanh",
+    title: "CREDITYES",
+    duyetText: "📌 BAO NỢ XẤU",
+    amount: "500.000 – 10.000.000",
+    features: [
+      "Thời hạn vay: 1 – 365 ngày",
+      "<strong>Lãi suất:Lãi suất: 0.01%/tháng</strong>",
+      "<strong>Không chứng minh thu nhập</strong>",
+      "Từ 22-60 tuổi trở lên",
+      "Hồ sơ: chỉ cần CMND",
+    ],
+    affiliateLink: "https://go.dinos.click/click?a=11826&o=1085",
+    label: "CreditYes"
+  },
+];
