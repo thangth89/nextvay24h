0bccca039e31b3d6fd212d18bfa18ff97151c19e
thangth89 <thangth.vinfast@gmail.com>
2025-07-21 01:04:27 +0700

    Update AffiliateButton.tsx

--- src/components/AffiliateButton.tsx
+++ src/components/AffiliateButton.tsx
@@ -84,7 +84,7 @@ export default function AffiliateButton({
 
         fbq('trackCustom', `Click_${label.replace(/\s+/g, '')}`);
       }
-    } catch (_) {
+    } catch () {
       // fail silently
     }
 
